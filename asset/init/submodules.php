fbddcc4220a1f9bba79479d01c359d90187d4bf2
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-27 18:40:09 +0900

    Doc: asset > init > submodules.php | PHPDoc

--- asset/init/submodules.php
+++ asset/init/submodules.php
@@ -1,27 +1,28 @@
 <?php
-/** op-skeleton-2024:/asset/git/init.php
+/**	op-skeleton-model:/asset/init/submodules.php
  *
- * Init the onepiece-framework.
+ * Init the ONEPIECE Framework.
  *
  * <pre>
  * ```sh
- * php asset/git/init.php
+ * php asset/init/submodules.php
  * ```
  * </pre>
  *
  * @created    2024-04-16
  * @version    1.0
- * @package    op-skeleton-2024
- * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @package    op-skeleton
+ * @subpackage model
+ * @author     Tomoaki Nagahara
  * @copyright  Tomoaki Nagahara All right reserved.
  */
 
-/** Declare strict
+/**	Declare strict
  *
  */
 declare(strict_types=1);
 
-/** namespace
+/**	namespace
  *
  */
 namespace OP;
