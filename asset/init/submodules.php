747cc4a43232f608e28d68dd79f8262294617bd7
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-27 19:00:24 +0900

    Add: asset > init > submodules.php | Switch branch each submodules is disabled

--- asset/init/submodules.php
+++ asset/init/submodules.php
@@ -56,6 +56,7 @@ $configs = GitSubmoduleConfig('.gitmodules', $git_root);
 //	Include op-skeleton config
 require_once("{$git_root}/asset/config/op.php");
 
+if( false ){
 //	Switch branch.
 foreach( $configs as $config ){
 	//	...
@@ -83,4 +84,5 @@ foreach( $configs as $config ){
 	Execute("git checkout origin/{$branch} -b {$branch}");
 	Execute("git branch -D root");
 	*/
+}
 }
