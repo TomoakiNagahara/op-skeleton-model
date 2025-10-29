6bc7f9a4fb2b9cf52379d2f42130ee250c1a79b7
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-30 00:29:08 +0900

    Chg: asset > init > function > GitSubmoduleForeach.php | Nested submodules are now supported.

--- asset/init/function/GitSubmoduleForeach.php
+++ asset/init/function/GitSubmoduleForeach.php
@@ -19,31 +19,43 @@ declare(strict_types=0);
  */
 namespace OP\SKELETON\INIT;
 
+//	...
+require_once(__DIR__.'/GitSubmoduleConfig.php');
+require_once(__DIR__.'/GitCheckoutTargetBranch.php');
+
 /**	Git submodule foreach.
  *
  * @created    2025-07-03
  * @param      string     $git_root
- * @param      array      $configs
  */
-function GitSubmoduleForeach( string $git_root, array $configs )
+function GitSubmoduleForeach( string $git_root )
 {
+	//	Save current directory.
+	$save_dir = getcwd();
+
+	//	Get git submodule config.
+	$configs = GitSubmoduleConfig('.gitmodules', $git_root);
+
 	//	Switch branch.
 	foreach( $configs as $config ){
 		//	...
 		$path   = $config['path'];
-		$branch = $config['branch'] ?? null;
+		$remote = $config['remote'] ?? 'origin';
+		$branch = $config['branch'] ?? _OP_APP_BRANCH_;
 
 		//	...
-		if(!$branch ){
-			$branch = _OP_APP_BRANCH_;
-		}
-
-		//	...
-		chdir($git_root);
-		chdir($path);
+		chdir("$git_root/$path");
 		echo getcwd() ." --> {$branch}". PHP_EOL;
 
 		//	...
-		GitCheckoutTargetBranch( $branch );
+		GitCheckoutTargetBranch( $remote, $branch );
+
+		//	...
+		if( $config['submodule'] ?? null ){
+			GitSubmoduleForeach("$git_root/$path");
+		}
 	}
+
+	//	Recovery current directory.
+	chdir($save_dir);
 }
