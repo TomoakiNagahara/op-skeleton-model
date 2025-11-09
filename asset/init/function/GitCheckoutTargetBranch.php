cdb79f3044a7b98067303c095bc984bca92da64d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-09 14:23:30 +0900

    Chg: asset > init > function > GitCheckoutTargetBranch.php | Can specified remote name now.

--- asset/init/function/GitCheckoutTargetBranch.php
+++ asset/init/function/GitCheckoutTargetBranch.php
@@ -27,18 +27,18 @@ require_once(__DIR__.'/Execute.php');
  * @created    2025-07-03
  * @param      string     $branch
  */
-function GitCheckoutTargetBranch( string $branch )
+function GitCheckoutTargetBranch( string $remote, string $branch )
 {
 	//	Check if branch exists.
-	if(!Execute("git show-ref --verify refs/remotes/origin/{$branch}") ){
-		Execute("git checkout origin/main -b {$branch}");
-		echo "\n * This branch has not been exist: {$branch} \n\n";
+	if(!Execute("git show-ref --verify refs/remotes/{$remote}/{$branch}") ){
+		Execute("git checkout {$remote}/main -b {$branch}");
+		echo "\n * This branch has not been exist: {$remote}/{$branch} \n\n";
 		return;
 	}
 
 	//	If switch fails, doing checkout.
 	if(!Execute("git switch {$branch}" )){
-		Execute("git checkout -b {$branch} origin/{$branch}");
+		Execute("git checkout -b {$branch} {$remote}/{$branch}");
 		Execute("git branch");
 	} // This comment out for git diff.
 }
